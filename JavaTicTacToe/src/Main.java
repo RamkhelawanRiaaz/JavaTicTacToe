8b93d9a088018916c6f9a137e325be163290d0b0
Ramkhelawan Riaaz <rakhelawanriaaz6@gmail.com>
2024-05-09 17:31:06 -0300

    test GitHub 2

--- JavaTicTacToe/src/Main.java
+++ JavaTicTacToe/src/Main.java
@@ -1,104 +1,15 @@
-import java.util.Scanner;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static char[][] board = new char[3][3];
-    private static char currentPlayer = 'X';
-
     public static void main(String[] args) {
-        initializeBoard();
-        displayBoard();
-        while (!isGameOver()) {
-            playTurn();
-            displayBoard();
-            switchPlayer();
-        }
-        char winner = checkWinner();
-        if (winner == ' ') {
-            System.out.println("It's a draw!");
-        } else {
-            System.out.println("Player " + winner + " wins!");
-        }
-    }
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-    private static void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = ' ';
-            }
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
-    }
-
-    private static void displayBoard() {
-        System.out.println("-------------");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
-        }
-    }
-
-    private static void playTurn() {
-        Scanner scanner = new Scanner(System.in);
-        int row, col;
-        do {
-            System.out.println("Player " + currentPlayer + ", enter your move (row[1-3] col[1-3]): ");
-            row = scanner.nextInt() - 1;
-            col = scanner.nextInt() - 1;
-        } while (!isValidMove(row, col));
-        board[row][col] = currentPlayer;
-    }
-
-    private static boolean isValidMove(int row, int col) {
-        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
-            System.out.println("Invalid move! Try again.");
-            return false;
-        }
-        return true;
-    }
-
-    private static void switchPlayer() {
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    }
-
-    private static boolean isGameOver() {
-        if (checkWinner() != ' ' || isBoardFull()) {
-            return true;
-        }
-        return false;
-    }
-
-    private static char checkWinner() {
-        // Check rows
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
-                return board[i][0];
-            }
-        }
-        // Check columns
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ') {
-                return board[0][j];
-            }
-        }
-        // Check diagonals
-        if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') ||
-                (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')) {
-            return board[1][1];
-        }
-        return ' ';
-    }
-
-    private static boolean isBoardFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == ' ') {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
