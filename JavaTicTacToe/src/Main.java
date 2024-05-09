f157b41065e9e8b3e1495a60e5f4fc64824af470
osamuels <osamuels@cbvs.sr>
2024-05-09 14:56:55 -0300

    Login deels af - still working on it

--- JavaTicTacToe/src/Main.java
+++ JavaTicTacToe/src/Main.java
@@ -1,15 +1,81 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static final String HackerGreen = "#149414";
+
+
+    public static void main(String[] args) {
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Welkom bij TikTacToe, made by Rammy Group");
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        AccountCheck();
+    }
+
+
+    public static void AccountCheck(){
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Heeft u nog geen account type Registreren | heeft u wel type Inloggen");
+        String Isregistered = myObj.nextLine();
+
+            if(Isregistered.equalsIgnoreCase("registreren")){
+                GebruikersRegistratie();
+            }
+            else if(Isregistered.equalsIgnoreCase("inloggen")) {
+                Inloggen();
+
+            }
+            else{
+                System.out.println("Ongeldige invoer probeer opnieuw");
+                AccountCheck();
+        }
+
+    }
+
+
+    public static void Inloggen(){
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Login");
+
+        System.out.println("Username");
+        String userName = myObj.nextLine();
+
+        System.out.println("Password");
+        String Password = myObj.nextLine();
+
+    }
+
+
+
+    public static void GebruikersRegistratie(){
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Registreer je nu!");
+
+        System.out.println("Voornaam");
+        String Voornaam = myObj.nextLine();
+
+        System.out.println("Achternaam");
+        String Achternaam = myObj.nextLine();
+
+        System.out.println("Gebruikersnaam");
+        String Gebruikersnaam = myObj.nextLine();
+
+        System.out.println("Wachtwoord");
+        String Password = myObj.nextLine();
+
+        System.out.println("GeboorteDatum");
+        String GeboorteDatum = myObj.nextLine();
+
+        System.out.println("Gaat u akkoord met onze community guidelines? (Ja/Nee)");
+        String Guidelines = myObj.nextLine();
     }
 }
